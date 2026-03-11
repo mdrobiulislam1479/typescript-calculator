e4281505dd385b6a969197bdeebfe98a0a9e314d
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-03-11 09:39:44 +0600

    added divide function

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,3 +5,10 @@ function add(a: number, b: number): number {
 const subtract = (a: number, b: number): number => a - b;
 
 const multiply = (a: number, b: number): number => a * b;
+
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error("Cannot divide by Zero");
+  }
+  return a / b;
+}
